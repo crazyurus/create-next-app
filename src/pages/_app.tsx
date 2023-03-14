7c4d6ac48c6a60e11582effaa781f2dcba22fa8e
Crazy Urus <crazyurus@vip.qq.com>
2023-03-15 00:49:00 +0800

    fix: next warning 的问题

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,9 @@ import Head from 'next/head';
 import Layout from '../layouts/Basic';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-
 import '../styles/global.scss';
+
+const defaultTitle = 'React App with Next.js';
 
 type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: JSX.Element, props: P) => JSX.Element;
@@ -22,12 +23,18 @@ function getDefaultLayout(page: JSX.Element, pageProps: any): JSX.Element {
 }
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
-  const { title } = pageProps;
+  const { title = defaultTitle } = pageProps;
   const getLayout = Component.getLayout ?? getDefaultLayout;
 
   return (
     <Fragment>
-      <Head>{title ? <title>{title}</title> : null}</Head>
+      <Head>
+        <title>{title}</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
       {getLayout(<Component {...pageProps} />, pageProps)}
     </Fragment>
   );
